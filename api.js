fe3fe7bc6690109a94cb171ed0da228621ba17b4
hangunhee94 <webzen9422@gmail.com>
2022-05-19 09:42:16 +0900

    update: 4-3강

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+const backend_base_url = "http://127.0.0.1:5001"
+const frontend_base_url = "http://127.0.0.1:5501"
+
 async function handleSignin() {
 
     const signupData = {
@@ -5,16 +8,67 @@ async function handleSignin() {
         password: document.getElementById("floatingPassword").value
     }
 
-    const response = await fetch('http://127.0.0.1:5001/signup', {
+    const response = await fetch(`${backend_base_url}/signup`, {
         method: 'POST',
         body: JSON.stringify(signupData)
     }
     )
+    console.log(response)
+
+    response_json = await response.json()
+    console.log(response_json)
+
     if (response.status == 200) {
-        window.location.replace('http://127.0.0.1:5500/login.html')
+        window.location.replace(`${frontend_base_url}/login.html`)
     } else {
         alert(response.status)
     }
+}
 
+async function handleLogin() {
+    console.log("handle Login")
+
+    const loginData = {
+        email: document.getElementById("floatingInput").value,
+        password: document.getElementById("floatingPassword").value
+    }
+
+    const response = await fetch(`${backend_base_url}/login`, {
+        method: 'POST',
+        body: JSON.stringify(loginData)
+    }
+    )
+
+
+    console.log(response)
+
+    response_json = await response.json()
+    console.log(response_json)
+    localStorage.setItem("token", response_json.token)
+
+
+    // if (response.status == 200) {
+    //     window.location.replace('http://127.0.0.1:5501/login.html')
+    // } else {
+    //     alert(response.status)
+    // }
+
+}
+
+async function getName() {
+
+    const response = await fetch(`${backend_base_url}/getuserinfo`, {
+        headers: {
+            'Authorization': localStorage.getItem("token")
+        }
+    }
+    )
+    response_json = await response.json()
+    console.log(response_json)
+
+    const username = document.getElementById("username")
+    username.innerText = response_json.email
+
+    // return response_json.email
 
 }
